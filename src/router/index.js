ff176a9e71c85cf9999c77ce59676edac7d08d08
Jonham <jonham@qq.com>
2017-02-19 16:28:22 +0800

    FieldHeader added
    add less dependances and make first page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// components
 import Hello from 'components/Hello'
+import PadField from 'components/PadField'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/pad',
+      name: 'PadField',
+      component: PadField
     }
   ]
 })
